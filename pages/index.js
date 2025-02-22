c513e41d6d52a91e9a9e798ed9637fd37358c022
lirajohny <johnylira.dev@gmail.com>
2025-02-22 15:39:51 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){// Componente react retornando uma texto para pagina
+	return (<h1>Página Inicial!</h1>)
+}
+
+export default Home;// sinaliza que esse componente deve ser iniciado primeiro
