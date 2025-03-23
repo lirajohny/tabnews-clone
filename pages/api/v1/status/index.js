314d25bb0c9c9c87a84d5786fa74886496e15a7f
lirajohny <johnylira.dev@gmail.com>
2025-03-23 15:31:33 -0300

    finish `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,32 @@
-function status(request, response) {
-  response.status(200).json({ nome: "johny", idade: 29, profissao: "programador" });
+import database from "infra/database.js"
+
+async function status(request, response) {
+  const databaseName = process.env.POSTGRES_DB;
+  const updatedAt = new Date().toISOString();
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const parsedMaxConnections = parseInt(maxConnections.rows[0].max_connections);
+
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1",
+    values: [databaseName],
+  });
+  const parsedOpenedConnections = openedConnections.rows[0].count;
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const parsedDbVersion = dbVersion.rows[0].server_version;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        status: "healthy",
+        max_connections: parsedMaxConnections,
+        opened_connections: parsedOpenedConnections,
+        version: parsedDbVersion,
+      }
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,5 +2,18 @@
 test("GET to status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status"); // faz a requisição do tipo async (assincrona) para esperar `await` a resposta do fetch
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // testa se o time é valido
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // Conexões max_connections
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  // Quantas conexões estão sendo usadas
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+  // Versão do Postgres
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
 });
 
